a90617dc57a4fb876864f9c1e3f3a58846b98ec3
Chernikov Nikita <14nik20@gmail.com>
2023-10-03 01:23:01 +0300

    Added movements and jumps

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    [SerializeField] public float MovementSpeed = 2.5F;
+    [SerializeField] public float JumpHeight = 2.25F;
+    [SerializeField] public float DistanceForGroundCheck = 0.15F;
+    [SerializeField] public LayerMask GroundLayerMask;
+
+    [SerializeField] public float Radius = 0.5F;
+
+    private float _moveX;
+    private bool _isJumping;
+    private float _jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _jumpForce = Mathf.Sqrt(JumpHeight * -2 * (Physics2D.gravity.y * _rigidbody.gravityScale));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveX = Input.GetAxisRaw("Horizontal");
+        if (!_isJumping && Input.GetButtonDown("Jump"))
+        {
+            _isJumping = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(_moveX * MovementSpeed, _rigidbody.velocity.y);
+
+        if (_isJumping && IsGrounded())
+        {
+            _rigidbody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+            _isJumping = false;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.CircleCast(transform.position, Radius, Vector2.down, DistanceForGroundCheck, GroundLayerMask);
+    }
+}
